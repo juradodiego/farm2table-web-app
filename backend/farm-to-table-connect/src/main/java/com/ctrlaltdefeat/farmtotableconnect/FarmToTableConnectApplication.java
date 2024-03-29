aa16ff03f48ef8027bdb0e6f9c55adf7d8103d25
Diego Jurado <diego.jurado@ieee.org>
2024-03-29 19:06:20 -0400

    Spring has sprung

---
+++ backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/FarmToTableConnectApplication.java
@@ -0,0 +1,13 @@
+package com.ctrlaltdefeat.farmtotableconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmToTableConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmToTableConnectApplication.class, args);
+	}
+
+}
---
+++ backend/farm-to-table-connect/src/test/java/com/ctrlaltdefeat/farmtotableconnect/FarmToTableConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctrlaltdefeat.farmtotableconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmToTableConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
