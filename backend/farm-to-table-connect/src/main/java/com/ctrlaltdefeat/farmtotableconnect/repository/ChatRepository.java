8485ba4d63329af5bdd823f1236a00ffed2bf651
Diego Jurado <79292142+juradodiego@users.noreply.github.com>
2024-03-30 07:39:05 -0400

    Update ChatRepository.java

--- backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/ChatRepository.java
+++ backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/ChatRepository.java
@@ -3,5 +3,6 @@ package com.ctrlaltdefeat.farmtotableconnect.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ctrlaltdefeat.farmtotableconnect.model.Chat;
 
+@Repository
 public interface ChatRepository extends JpaRepository<Chat, Integer>{
 }
