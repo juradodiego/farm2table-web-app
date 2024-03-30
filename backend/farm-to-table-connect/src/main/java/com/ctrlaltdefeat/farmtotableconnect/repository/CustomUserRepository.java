2e34452beece2a6f6f109c223d0d6fb463ad1390
Diego Jurado <diego.jurado@ieee.org>
2024-03-29 21:54:27 -0400

    Implementing CustomUserRepository

--- backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/CustomUserRepository.java
+++ backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/CustomUserRepository.java
@@ -1,12 +1,12 @@
 package com.ctrlaltdefeat.farmtotableconnect.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ctrlaltdefeat.farmtotableconnect.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>, CustomUserRepository {
-    
-
+public interface CustomUserRepository {
+    public User getUserByID(Integer userID);
+    public User getUserByName(String username);
+    public User createNewUser(User user);
 }
--- backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/impl/UserRepositoryImpl.java
+++ backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/impl/UserRepositoryImpl.java
@@ -1,5 +1,27 @@
 package com.ctrlaltdefeat.farmtotableconnect.repository.impl;
 
-public class UserRepositoryImpl {
+import com.ctrlaltdefeat.farmtotableconnect.model.User;
+import com.ctrlaltdefeat.farmtotableconnect.repository.CustomUserRepository;
+import com.ctrlaltdefeat.farmtotableconnect.repository.UserRepository;
+
+public class UserRepositoryImpl implements CustomUserRepository {
+
+    @Override
+    public User getUserByID(Integer userID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUserByID'");
+    }
+
+    @Override
+    public User getUserByName(String username) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUserByName'");
+    }
+
+    @Override
+    public User createNewUser(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createNewUser'");
+    }
     
 }
