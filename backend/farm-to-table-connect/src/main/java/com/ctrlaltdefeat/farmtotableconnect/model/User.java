5e184459121020926d96bd46b5208b64eff0c738
estJune <92344421+estJune@users.noreply.github.com>
2024-03-29 21:26:11 -0400

    created model.User

--- backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/model/User.java
+++ backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/model/User.java
@@ -1,5 +1,32 @@
 package com.ctrlaltdefeat.farmtotableconnect.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GeenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Integer user_id;
+    private String username;
+    private Integer farm_id;
+    private String address_line;
+    private String city;
+    private String state;
+    private int zipcode;
+    private Boolean consumer;
+    private String email;
+    private Integer salt;
+    private String password;
 }
--- backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/service/UserSerivce.java
+++ backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/service/UserSerivce.java
@@ -1,5 +1,9 @@
 package com.ctrlaltdefeat.farmtotableconnect.service;
 
-public class UserSerivce {
-    
+import com.ctrlaltdefeat.farmtotableconnect.model.User;
+
+public interface UserSerivce {
+    public User getUser(String username, String password);
+    public User createUser(User user);
+    public User updateUser(User user);
 }
