626a4b0591b433fd029c2910ad15c393eaf72512
Maanya Shanker <maanyashanker30@gmail.com>
2024-03-29 21:20:08 -0400

    finished user functions

--- backend/operations.sql
+++ backend/operations.sql
@@ -7,7 +7,7 @@ set schema 'farm2table';
 -------------------------------------------
 -- getUser(username)
 -------------------------------------------
-create or replace function getUser(uname varchar(20))
+create or replace function getUserByUsername(uname varchar(20))
     returns record as
     $$
     begin
@@ -19,7 +19,7 @@ create or replace function getUser(uname varchar(20))
 -------------------------------------------
 -- getUser(userid)
 -------------------------------------------
-create or replace function getUser(uid varchar(20))
+create or replace function getUserByUserId(uid varchar(20))
     returns record as
     $$
     begin
@@ -30,23 +30,44 @@ create or replace function getUser(uid varchar(20))
 
 -------------------------------------------
 -- getUser(user_id integer,
---   username varchar(20) not null,
+--   username varchar(20),
 --   farm_id integer,
---   address_line varchar(60) not null,
---   city varchar(30) not null,
---   state varchar(2) not null,
---   zipcode int not null,
---   consumer boolean not null,
---   email varchar(60) not null unique,
---   salt integer not null,
---   password text not null,
---   farm_name varchar(60),)
+--   address_line varchar(60),
+--   city varchar(30),
+--   state varchar(2),
+--   zipcode int,
+--   consumer boolean,
+--   email varchar(60),
+--   salt integer,
+--   password text,
+--   farm_name)
 -------------------------------------------
--- create or replace function getUser(uid varchar(20))
---     returns record as
---     $$
---     begin
---         return (select * from farm2table.users where users.user_id = uid);
---     end;
---     $$
---     language plpgsql;
+create or replace function getUser(user_id integer,
+  username varchar(20),
+  farm_id integer,
+  address_line varchar(60),
+  city varchar(30),
+  state varchar(2),
+  zipcode int,
+  consumer boolean,
+  email varchar(60),
+  salt integer,
+  password text,
+  farm_name varchar(60))
+    returns boolean as
+    $$
+    declare
+        row_count_before int;
+        row_count_after int;
+    begin
+        select count(*) into row_count_before from users;
+        insert into farm2table.users(values(user_id, username, farm_id, address_line, city, state,
+                                            zipcode, consumer, email, salt, password, farm_name));
+        if row_count_after != row_count_before then
+            return true;
+        else
+            return false;
+        end if;
+    end;
+    $$
+    language plpgsql;
