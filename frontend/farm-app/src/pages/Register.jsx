ae7987c686f4b80989cf05815eda47556af9742d
dwm39 <dwm39@pitt.edu>
2024-03-29 21:55:07 -0400

    finished login and register html (kill me)

--- frontend/farm-app/src/pages/Register.jsx
+++ frontend/farm-app/src/pages/Register.jsx
@@ -3,25 +3,28 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "../style/register.scss";
 import Add from "../img/addAvatar.png";
-import "../dropdown";
+import "../style/dropdown.js";
 
 const Register = () => {
   const [isFarmer, setFarmer] = useState(false);
+  const [clicked, setClicked] = useState(false);
   return (
     <div className="formContainer2">
       <div className="formWrapper">
         <div className="button">
           <button
             onClick={() => {
-              useState(true);
+              setFarmer(true);
             }}
+            style={{ backgroundColor: isFarmer ? "#283618" : "#b5651d" }}
           >
             Farmer
           </button>
           <button
             onClick={() => {
-              useState(false);
+              setFarmer(false);
             }}
+            style={{ backgroundColor: !isFarmer ? "#283618" : "#b5651d" }}
           >
             Customer
           </button>
@@ -29,43 +32,45 @@ const Register = () => {
         <span className="title"> Register</span>
         <span className="logo">Farm 2 Table Connect</span>
         <form onSubmit={() => {}}>
-          <input type="text" placeholder="display name" />
+          <input type="text" placeholder="First Name, Last Name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
-          <input type="location" placeholder="location" />
-          <div id="list1" class="dropdown-check-list" tabindex="100">
-            <span class="anchor">Select Fruits</span>
-            <ul class="items">
-              <li>
-                <input type="checkbox" />
-                Apple{" "}
-              </li>
-              <li>
-                <input type="checkbox" />
-                Orange
-              </li>
-              <li>
-                <input type="checkbox" />
-                Grapes{" "}
-              </li>
-              <li>
-                <input type="checkbox" />
-                Berry{" "}
-              </li>
-              <li>
-                <input type="checkbox" />
-                Mango{" "}
-              </li>
-              <li>
-                <input type="checkbox" />
-                Banana{" "}
-              </li>
-              <li>
-                <input type="checkbox" />
-                Tomato
-              </li>
-            </ul>
-          </div>
+          {isFarmer && (
+            <div>
+              <input type="location" placeholder="location" />
+              <div className="multiselect">
+                <div
+                  className="selectBox"
+                  onClick={() => {
+                    setClicked(!clicked);
+                  }}
+                >
+                  <select>
+                    <option>Select Produce to Sell</option>
+                  </select>
+                  <div className="overSelect"></div>
+                </div>
+                <div id="checkboxes">
+                  {clicked && (
+                    <div className="boxes">
+                      <label htmlFor="one">
+                        <input type="checkbox" id="one" />
+                        <p>Watermelon</p>
+                      </label>
+                      <label htmlFor="two">
+                        <input type="checkbox" id="two" />
+                        <p>Blueberries</p>
+                      </label>
+                      <label htmlFor="three">
+                        <input type="checkbox" id="three" />
+                        <p>Strawberries</p>
+                      </label>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          )}
           <input style={{ display: "none" }} type="file" id="file" />
           <label htmlFor="file">
             <img src={Add} alt="" />
