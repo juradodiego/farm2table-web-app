2cd99f129f1bcbad5cffa7ffb575db9b9b18237c
Diego Jurado <79292142+juradodiego@users.noreply.github.com>
2024-03-30 07:38:50 -0400

    Update ProduceRepository.java

--- backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/ProduceRepository.java
+++ backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/ProduceRepository.java
@@ -2,7 +2,7 @@ package com.ctrlaltdefeat.farmtotableconnect.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ctrlaltdefeat.farmtotableconnect.model.Produce;
-
+@Repository
 public interface ProduceRepository extends JpaRepository<Produce, Integer>{
     
 }
