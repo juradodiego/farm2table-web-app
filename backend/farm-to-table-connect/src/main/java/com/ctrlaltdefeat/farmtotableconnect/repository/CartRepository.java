b622ba0bbf5488bb4030d90d1d6d9cabf46ca559
Diego Jurado <79292142+juradodiego@users.noreply.github.com>
2024-03-30 07:39:12 -0400

    Update CartRepository.java

--- backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/CartRepository.java
+++ backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/CartRepository.java
@@ -3,6 +3,7 @@ package com.ctrlaltdefeat.farmtotableconnect.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ctrlaltdefeat.farmtotableconnect.model.Cart;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Integer>{
     
 }
