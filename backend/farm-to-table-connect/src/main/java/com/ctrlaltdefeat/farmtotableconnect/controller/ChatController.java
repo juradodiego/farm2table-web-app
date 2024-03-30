9cfe5a15c01408b4d06f98a3eb5865125425b880
estJune <92344421+estJune@users.noreply.github.com>
2024-03-30 09:15:03 -0400

    finished chat controller

--- backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/controller/ChatController.java
+++ backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/controller/ChatController.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ctrlaltdefeat.farmtotableconnect.service.ChatService;
 import com.ctrlaltdefeat.farmtotableconnect.service.UserService;
 import com.ctrlaltdefeat.farmtotableconnect.model.Chat;
+import com.ctrlaltdefeat.farmtotableconnect.model.Cart;
 import com.ctrlaltdefeat.farmtotableconnect.model.Message;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.aspectj.apache.bcel.classfile.Module.Open;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,15 +24,15 @@ import java.util.List;
 
 
 
-@ClassOrigin(origins="http://localhoost::5176")
+@CrossOrigin(origins="http://localhos::5176")
 @RestController
-@RequestMapping("path")
+@RequestMapping("chat")
 public class ChatController {
     
     @Autowired
     private ChatService chatService;
     
-    @GetMapping("/")
+    @GetMapping("/getChat")
     public Chat getChat(@RequestParam Integer customerId, Integer farmerId) {
         Chat chat = chatService.getChat(customerId, farmerId);
         if (chat == null) {
@@ -38,7 +41,7 @@ public class ChatController {
         return chat;
     }
 
-    @PostMapping("/")
+    @GetMapping("/getLatest")
     public List<Message> getLatest(@RequestParam Integer chatId, Timestamp latestTimestamp) {
         List<Message> chat = chatService.getLatest(chatId, latestTimestamp);
         if (chat == null) {
@@ -47,44 +50,22 @@ public class ChatController {
         return chat;
     }
     
-    @PostMapping("path")
-    public Boolean sendMessage(@RequestBody Message message) {
-        Boolean chat = chatService.sendMessage(message);
-        if (chat == false) {
+    @PostMapping("/sendMessage")
+    public Chat sendMessage(@RequestBody Message message) {
+        Chat chat = chatService.sendMessage(message);
+        if (chat == null) {
             throw new Exception("Cannot send message");
         }
         return chat;
     }
 
-    @PostMapping("path")
-    public Boolean finishConveration(@RequestBody Integer chatId) {
-        Boolean chat = chatService.finishConversation(chatId);
-        if (chat == false) {
+    @PostMapping("/finishConversation")
+    public Cart finishConveration(@RequestBody Integer chatId) {
+        Cart chat = chatService.finishConversation(chatId);
+        if (chat == null) {
             throw new Exception("Cannot finish conversation");
         }
         
         return chat;
     }
-
-    @PostMapping("path")
-    public Boolean isFarmerApproved(@RequestBody Integer chatId) {
-        Boolean chat = chatService.isFarmerApproved(chatId);
-        if (chat == false) {
-            throw new Exception("Cannot validate farmer");
-        }
-        
-        return chat;
-    }
-
-    @PostMapping("path")
-    public Boolean isCustomerApproved(@RequestBody Integer chatId) {
-        Boolean chat = chatService.isCustomerApproved(chatId);
-        if (chat == false) {
-            throw new Exception("Cannot validate farmer");
-        }
-        
-        return chat;
-    }
-    
-    
 }
