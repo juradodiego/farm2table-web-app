aa6c74d53bda9cb721f2e444b38e52cc92b38efd
Diego Jurado <79292142+juradodiego@users.noreply.github.com>
2024-03-30 07:38:59 -0400

    Update FarmRepository.java

--- backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/FarmRepository.java
+++ backend/farm-to-table-connect/src/main/java/com/ctrlaltdefeat/farmtotableconnect/repository/FarmRepository.java
@@ -4,6 +4,7 @@ package com.ctrlaltdefeat.farmtotableconnect.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ctrlaltdefeat.farmtotableconnect.model.Farm;
 
+@Repository
 public interface FarmRepository extends JpaRepository<Farm, Integer>{
 
     
